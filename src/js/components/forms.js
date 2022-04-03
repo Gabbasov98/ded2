bbd52abd7034a0811d38740603db206baddd14cb
Marsel Gabbasov <marsel13_98@mail.ru>
2022-04-03 16:33:01 +0300

    sdff

--- src/js/components/forms.js
+++ src/js/components/forms.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
 
     $(".validate-form button[type='submit']").click(function(e) {
         e.preventDefault()
+        let agree = $(this).parents("form").find(".agree-check")
 
+        if (!$(agree).prop("checked")) {
+            $(agree).parents(".check").addClass("check--error")
+            return
+        }
+        $(agree).parents(".check").removeClass("check--error")
         $(this).parents("form").find(".required").each(function(index, el) {
             if ($(el).val()) {
                 $(el).parent(".form-group").removeClass("form-group--error")
